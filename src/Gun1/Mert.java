762e1fb00680914fa6f603ad08ba15a0b1089e79
Muhammmed <muhammeduyar42@gmail.com>
2024-03-06 22:52:03 +0300

    Githubdaki ilk commitim

---
+++ src/Gun1/Mert.java
@@ -0,0 +1,9 @@
+package Gun1;
+
+public class Mert {
+    public static void main(String[] args) {
+        System.out.println("sampiyon cimbom");
+
+        System.out.println("merhaba");
+    }
+}
